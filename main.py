4c3d4bb6281943ca17f0c6283fcfac1cc5f15335
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2021-05-15 14:19:55 +0900

    download album images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import requests
 import spotipy
+from PIL import Image
 from spotipy.oauth2 import SpotifyOAuth
 from pprint import pprint
 
@@ -12,17 +14,20 @@ def main():
     for i, playlist in enumerate(playlists):
         print(f"{i}, {playlist['name']}, {playlist['id']}")
 
-    tracks = sp.playlist("XXX")["tracks"]["items"]
+    tracks = sp.playlist("")["tracks"]["items"]
     for track in tracks:
         print("============================")
         track_name = track["track"]["name"]
         image_url = track["track"]["album"]["images"][0]["url"]
+        album_name = track["track"]["album"]["name"]
         artists = track["track"]["artists"]
 
-        print(track_name, image_url)
+        print(f"{album_name}, {track_name}, {image_url}")
         for artist in artists:
             print(artist["name"])
 
+        album_image = Image.open(requests.get(image_url, stream=True).raw)
+        album_image.save(f"./images/{album_name}.jpg")
 
 
 if __name__ == "__main__":
