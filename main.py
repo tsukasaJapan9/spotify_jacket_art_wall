a76e2f9b4588372c3e7d70645e807917306155aa
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2021-05-15 14:03:23 +0900

    get album url

--- main.py
+++ main.py
@@ -8,8 +8,21 @@ def main():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
     playlists = sp.current_user_playlists()["items"]
-    for playlist in playlists:
-        print(f"{playlist['name']}, {playlist['id']}")
+    print("ID, playlist name, playlist id")
+    for i, playlist in enumerate(playlists):
+        print(f"{i}, {playlist['name']}, {playlist['id']}")
+
+    tracks = sp.playlist("XXX")["tracks"]["items"]
+    for track in tracks:
+        print("============================")
+        track_name = track["track"]["name"]
+        image_url = track["track"]["album"]["images"][0]["url"]
+        artists = track["track"]["artists"]
+
+        print(track_name, image_url)
+        for artist in artists:
+            print(artist["name"])
+
 
 
 if __name__ == "__main__":
