938170a23b9d3aa74758228f1332015c928e2001
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2021-05-15 13:27:57 +0900

    add sample

--- Makefile
+++ Makefile
@@ -2,7 +2,6 @@ run:
 	.venv/bin/python main.py
 
 setup:
-	rm -rf .venv
 	python -m venv .venv
 	.venv/bin/pip install -r requirements.txt
 
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
 
 
 def main():
-    print("test")
+    scope = "user-library-read"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    playlists = sp.current_user_playlists()["items"]
+    for playlist in playlists:
+        print(f"{playlist['name']}, {playlist['id']}")
 
 
 if __name__ == "__main__":
